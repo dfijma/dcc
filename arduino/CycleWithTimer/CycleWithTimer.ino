acea4bf5e2750ed5d014316c4f3e5b79fcca0dd8
Duco Fijma <d.fijma@activevideo.com>
2017-08-24 22:17:15 +0200

    train actually runs

--- arduino/CycleWithTimer/CycleWithTimer.ino
+++ arduino/CycleWithTimer/CycleWithTimer.ino
@@ -3,18 +3,6 @@
 // i2c used for input
 #define SLAVE_ADDRESS 0x04
 
-// buffer of DCC data to modulate
-// current work, a byte to send and a bitmask pointing to bit being sent
-// if currentBitMask is 0, we are idle
-// this is still TODO, just a single byte buffer at this time
-volatile byte currentByte;
-volatile byte currentBitMask;
-volatile byte buffered = B10101010;
-
-volatile byte currentByteProg;
-volatile byte currentBitMaskProg;
-volatile byte bufferedProg = B01010101;
-
 // DCC signal modulating
 
 // basic idea is as follows:
@@ -35,8 +23,12 @@ volatile byte bufferedProg = B01010101;
 // the DCC signals are, via hardware jumper, fed into direction pin of motor shield, actual direction output pin is to be disabled
 // main signal
 #define DIRECTION_MOTOR_CHANNEL_PIN_A 12
+#define SPEED_MOTOR_CHANNEL_PIN_A 3
+#define BRAKE_MOTOR_CHANNEL_PIN_A 9
 // prog signal
 #define DIRECTION_MOTOR_CHANNEL_PIN_B 13
+#define SPEED_MOTOR_CHANNEL_PIN_B 11
+#define BRAKE_MOTOR_CHANNEL_PIN_B 8
 
 // timer values for generating DCC ZERO and DCC ONE bits (ZERO cycle is 2 * 100 microseconds, ONE cycle is 2 * 58 microseconds)
 // values for 16 bit timer 1 based on 16Mhz clock and a 1:1 prescale
@@ -55,6 +47,9 @@ volatile byte bufferedProg = B01010101;
 
 void setup() {
   Serial.begin(9600);
+
+  // initialize refresh buffer (with an idle packet)
+  initRefreshBuffer();
  
   // initialize i2c as slave
   Wire.begin(SLAVE_ADDRESS);
@@ -117,12 +112,15 @@ void setup() {
   // enable interrup OC0B
   bitSet(TIMSK0,OCIE0B);
 
-  // init input buffers
-  currentByte = buffered;
-  currentBitMask = B10000000;
-
-  currentByteProg = bufferedProg;
-  currentBitMaskProg = B10000000;
+  // power on
+  pinMode(SPEED_MOTOR_CHANNEL_PIN_A,OUTPUT);   // main track power
+  digitalWrite(SPEED_MOTOR_CHANNEL_PIN_A, HIGH);
+  pinMode(BRAKE_MOTOR_CHANNEL_PIN_A,OUTPUT);  // release brake
+  digitalWrite(BRAKE_MOTOR_CHANNEL_PIN_A, LOW); 
+  pinMode(SPEED_MOTOR_CHANNEL_PIN_B,OUTPUT);   // prog track power
+  digitalWrite(SPEED_MOTOR_CHANNEL_PIN_B, HIGH);
+  pinMode(BRAKE_MOTOR_CHANNEL_PIN_B,OUTPUT);  // release brake
+  digitalWrite(BRAKE_MOTOR_CHANNEL_PIN_B, LOW); 
 }
 
 ISR(TIMER1_COMPB_vect){         
@@ -132,7 +130,7 @@ ISR(TIMER1_COMPB_vect){
   // are double latched (and only really loaded in the actual registers at next time timer flows over from TOP to BOTTOM
   
   // next bit, please
-  if (currentByte & currentBitMask) {
+  if (nextBit()) {
     // now we need to sent "1"
     // set OCR1A for next cycle to full cycle of DCC ONE bit
     // set OCR1B for next cycle to half cycle of DCC ONE bit
@@ -145,16 +143,6 @@ ISR(TIMER1_COMPB_vect){
     OCR1A=DCC_ZERO_BIT_TOTAL_DURATION_TIMER1;
     OCR1B=DCC_ZERO_BIT_PULSE_DURATION_TIMER1;
   }
-
-  currentBitMask = currentBitMask >> 1;
-
-  // if done, take next byte of work
-  // for, now a arbitrary byte if we run out of work
-  if (!currentBitMask) {
-    // take last sent byte
-    currentByte = buffered;
-    currentBitMask  = B10000000; // starting with MSB      
-  }  
 }
 
 // code duplication for handler for prog track is intentional
@@ -163,7 +151,7 @@ ISR(TIMER0_COMPB_vect) {
   // idea is exactly the same
   
   // next bit, please
-  if (currentByteProg & currentBitMaskProg) {
+  if (true /* todo: a prog track buffer */) {
     // now we need to sent "1"
     // set OCR1A for next cycle to full cycle of DCC ONE bit
     // set OCR1B for next cycle to half cycle of DCC ONE bit
@@ -176,44 +164,194 @@ ISR(TIMER0_COMPB_vect) {
     OCR0A=DCC_ZERO_BIT_TOTAL_DURATION_TIMER0;
     OCR0B=DCC_ZERO_BIT_PULSE_DURATION_TIMER0;
   }
-
-  currentBitMaskProg = currentBitMaskProg >> 1;
-
-  // if done, take next byte of work
-  // for, now a arbitrary byte if we run out of work
-  if (!currentBitMaskProg) {
-    // take last sent byte
-    currentByteProg = bufferedProg;
-    currentBitMaskProg  = B10000000; // starting with MSB      
-  }  
 }
  
 void loop() {
   // main loop is empty
 }
+
+// ack data from receive to transmit
+volatile byte ack;
  
 // callback for received data
 void receiveData(int byteCount) {
+  Serial.println("start receive");
   // read a single i2c block data transaction
   int count = 0;
   byte buffer[32];
-  
-  while (Wire.available() && count<32) {
-    buffer[count] = Wire.read();
+
+  // read the frame and keep max 32 bytes
+  while (Wire.available()) {
+    byte b  = Wire.read();
+    if (count < 32) buffer[count] = b;
     count++;
   }
-  Serial.print(count); Serial.print(": ");
+  Serial.print("receive: "); Serial.print(count); Serial.print(": ");
   for (int i=0; i<count; i++) {
     Serial.print(buffer[i]);
     Serial.print(" ");
   }
   Serial.println();
+
+  if (count < 2) return; // no enough data
+  // Just a basic interface to start:
+  // cmd: direction value: 0..126 or 
+  // data[0]: direction (0 is backwards, != 0 is forward
+
+  // create a DCC update throttle command
+  byte b[5]; // max 5 bytes, including checksum byte
+  int cab = 3; // TODO: for now, hard coded loc 3
+  int tSpeed =  buffer[0]; // 0..126 
+  if (tSpeed < 0) tSpeed = 0;
+  if (tSpeed > 126) tSpeed = 126;
+  int tDirection = (buffer[1] != 0); // 1 is forward, 0 is backward
+  byte nB=0;
+
+  if(cab>127)
+    b[nB++]=highByte(cab) | 0xC0;      // convert train number into a two-byte address
+    
+  b[nB++]=lowByte(cab);
+  b[nB++]=0x3F;                        // 128-step speed control byte
+  if(tSpeed>=0) 
+    // max speed is 126, but speed codes range from 2-127 (0=stop, 1=emergency stop)
+    // so map 0, 1, 2 .. 126 -> to 0, 2, 3, .. 127 and set high bit iff forward
+    b[nB++]=tSpeed+(tSpeed>0)+tDirection*128;
+  else{
+    b[nB++]=1; // emergency stop
+    tSpeed=0;
+  }
+
+  Serial.print("set speed bytes : ");
+  for (int i =0; i<nB; i++) {
+    Serial.print(b[i]); Serial.print(" ");
+  }
+  Serial.println();
+
+  updatePacket(b, nB); // which will be silently ignored if refresh buffer has no room
+
+  ack = 1; // success
 }
  
 // callback for sending data
 void sendData(){
-  Serial.println("asdf");
-  Wire.write(1);
-  Wire.write(42);
+  // Wire.write(ack);
 }
 
+// DCC packets and refresh buffer
+
+struct DCCPacket {
+  byte buffer[10];
+  byte bits;
+};
+
+byte idlePacket[3]={0xFF,0x00,0};
+byte testPacket[4] ={0x03, 0x3F, 0x9E}; // just to test, set speed of loco 3 to 0x8F = 15 + 128 = 15 forward of 0x9E = 30 forward
+
+struct RefreshRegister {
+  DCCPacket packet[2];
+  DCCPacket *activePacket;
+  DCCPacket *updatePacket;
+};
+
+// for now, a single register
+struct RefreshBuffer {
+  RefreshRegister reg;
+  byte currentBit;
+};
+
+volatile RefreshBuffer theBuffer;
+
+void initRefreshBuffer() {
+  // init
+  theBuffer.reg.packet[0].bits = 0;
+  theBuffer.reg.packet[1].bits = 0;
+  theBuffer.reg.activePacket = theBuffer.reg.packet; // first packet is active
+  theBuffer.reg.updatePacket = theBuffer.reg.packet + 1; // second is for update
+
+  theBuffer.reg.activePacket->bits = 0;
+  theBuffer.reg.updatePacket->bits = 0;
+  
+  // load an idle packet into activePacket
+  DCCbytesToPacket(idlePacket, 2, theBuffer.reg.activePacket);
+  // DCCbytesToPacket(testPacket, 3, theBuffer.reg.activePacket);
+
+  // next bit to play is first bit of activePacket
+  theBuffer.currentBit = 0;
+}
+
+void updatePacket(byte in[], byte nBytes) {
+  if (theBuffer.reg.updatePacket->bits != 0) return; // buffer full, for now, silently ignore
+  DCCbytesToPacket(in, nBytes, theBuffer.reg.updatePacket);
+}
+
+byte mask[] = {0x80,0x40,0x20,0x10,0x08,0x04,0x02,0x01};
+
+boolean nextBit() {
+  if (theBuffer.currentBit == theBuffer.reg.activePacket->bits) {  
+    // all bit is activePacket are done, copy next packet if available
+    if (theBuffer.reg.updatePacket->bits != 0) {
+      Serial.println("load new packet");
+      DCCPacket *tmp = theBuffer.reg.activePacket;
+      theBuffer.reg.activePacket = theBuffer.reg.updatePacket;
+      theBuffer.reg.updatePacket = tmp;
+      // and clear what is now updatePacket
+      theBuffer.reg.updatePacket->bits = 0;
+    } 
+    // and start streaming (new) activePacket or repeat to old activePacket
+    theBuffer.currentBit = 0;
+  }
+
+  boolean res = theBuffer.reg.activePacket->buffer[theBuffer.currentBit/8] & mask[theBuffer.currentBit%8];
+  theBuffer.currentBit++;
+  return res;
+}
+
+/*
+ * 3 bytes:
+
+22 preamble 
+ 1 packet start bit
+ 8 byte 1
+ 1 data start bit
+ 8 byte 2
+ 1 data start bit
+ 8 error detection (byte3)
+ 1 packet end bit
+
+ */
+
+void DCCbytesToPacket(byte in[], int nBytes,  struct DCCPacket *packet) {          
+  byte *out = packet->buffer;
+  in[nBytes]=in[0];                        // copy first byte into what will become the checksum byte  
+  for(int i=1;i<nBytes;i++)              // XOR remaining bytes into checksum byte
+    in[nBytes]^=in[i];
+  nBytes++;                              // increment number of bytes in packet to include checksum byte
+      
+  out[0]=0xFF;                         // first 8 bits of 22-byte preamble
+  out[1]=0xFF;                        // second 8 bits of 22-byte preamble
+  out[2]=0xFC + bitRead(in[0],7);      // last 6 bits of 22-byte preamble + data start bit + b[0], bit 7
+  out[3]=in[0]<<1;                     // in[0], bits 6-0 + data start bit
+  out[4]=in[1];                        // in[1], all bits
+  out[5]=in[2]>>1;                     // in[2], bits 7-1
+  out[6]=in[2]<<7;                     // in[2], bit 0
+
+  if (nBytes==3) {
+    packet->bits=49;
+  } else{
+    out[6]+=in[3]>>2;                  // in[3], bits 7-2
+    out[7]=in[3]<<6;                   // in[3], bit 1-0
+    if(nBytes==4){
+      packet->bits=58;
+    } else{
+      out[7]+=in[4]>>3;                // in[4], bits 7-3
+      out[8]=in[4]<<5;                 // in[4], bits 2-0
+      if(nBytes==5){
+        packet->bits=67;
+      } else{
+        out[8]+=in[5]>>4;              // in[5], bits 7-4
+        out[9]=in[5]<<4;               // in[5], bits 3-0
+        packet->bits=76;
+      } // >5 bytes
+    } // >4 bytes
+  } // >3 bytes
+} 
--- press.py
+++ press.py
@@ -10,14 +10,18 @@ import smbus
 bus = smbus.SMBus(1)
 address = 0x04
 
-def write(value):
-	test = [0xff, 0xff, 0xff, value]
-	bus.write_i2c_block_data(address, 0x42, test)
-        return -1
+def write(speed, direction):
+    print("set speed " + str(speed) + " direction " + str(direction))
+    dirval=0
+    if direction:
+    	dirval=1
+    data = [dirval]
+    bus.write_i2c_block_data(address, speed, data)
 
 def read():
-        result = bus.read_i2c_block_data(address,0x43)
-	print result
+    #result = bus.read_i2c_block_data(address,0x43)
+    #print result
+    pass
 
 # Define MCP pins connected to the LCD.
 lcd_rs        = 1
@@ -33,87 +37,87 @@ lcd_columns = 16
 lcd_rows    = 2
 
 class View:
-	
-	def __init__(self,model):
-		# Initialize MCP23017 device using its default 0x20 I2C address.
-		self.gpio = MCP.MCP23008(0x20, busnum=1)
-		self.lcd = LCD.Adafruit_CharLCD(lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6, lcd_d7,
+    
+    def __init__(self,model):
+    	# Initialize MCP23017 device using its default 0x20 I2C address.
+    	self.gpio = MCP.MCP23008(0x20, busnum=1)
+    	self.lcd = LCD.Adafruit_CharLCD(lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6, lcd_d7,
                               lcd_columns, lcd_rows, lcd_back,
                               gpio=self.gpio, invert_polarity=False)
-		self.led = LED(16)
-		self.model = model
-		self.formatter = "{:" + str(8) + "}"
-		self.redraw()
+    	self.led = LED(16)
+    	self.model = model
+    	self.formatter = "{:" + str(8) + "}"
+    	self.redraw()
 
-	def update(self):
-        	self.lcd.set_cursor(0,1)
-        	self.lcd.message(self.formatter.format(str(self.model.value)))
-		self.lcd.set_cursor(8,1)
-		if self.model.toggle:
-			self.lcd.message("forward ")
-			self.led.on()
-		else:
-			self.lcd.message("backward")
-			self.led.off()
-		
-	def redraw(self):
-		self.lcd.clear()
-		self.lcd.message("speed:  dir:")
-		self.update()
+    def update(self):
+        self.lcd.set_cursor(0,1)
+        self.lcd.message(self.formatter.format(str(self.model.value)))
+    	self.lcd.set_cursor(8,1)
+    	if self.model.toggle:
+    	    self.lcd.message("forward ")
+    	    self.led.on()
+    	else:
+    	    self.lcd.message("backward")
+    	    self.led.off()
+    	
+    def redraw(self):
+    	self.lcd.clear()
+    	self.lcd.message("speed:  dir:")
+    	self.update()
 
 class Model:
-	def __init__(self):
-		self.value = 0
-		self.toggle = True
+    def __init__(self):
+    	self.value = 0
+    	self.toggle = True
 
-	def inc(self):
-		self.value += 1
-		if self.value > 128:
-			self.value = 128
-		write(self.value)
-                read()
+    def inc(self):
+    	self.value += 10
+    	if self.value > 126:
+    	    self.value = 126
+    	write(self.value, self.toggle)
+        read()
 
-	def dec(self):
-		self.value -= 1
-		if self.value < 0:
-			self.value = 0
-		write(self.value)
-		read()	
+    def dec(self):
+    	self.value -= 10
+    	if self.value < 0:
+    		self.value = 0
+    	write(self.value, self.toggle)
+    	read()	
 
-	def switch(self):
-		if self.value == 0:
-			self.toggle = not(self.toggle)
-		else:
-			self.value=0
-                read()
-
+    def switch(self):
+    	if self.value == 0:
+    		self.toggle = not(self.toggle)
+    	else:
+    		self.value=0
+        write(self.value, self.toggle)
+    	read()
 
 class Controller:
 
-	def __init__(self,model,view):
-		self.model = model
-		self.view = view
-		self.button1 = Button(22)
-		self.button2 = Button(27)
-		self.button3 = Button(17)
-		self.button1.when_pressed=self.on_button1
-		self.button2.when_pressed=self.on_button2
-		self.button3.when_pressed=self.on_button3
-		
-	def on_button1(self):
-                model.dec()
-		view.update()
+    def __init__(self,model,view):
+    	self.model = model
+    	self.view = view
+    	self.button1 = Button(22)
+    	self.button2 = Button(27)
+    	self.button3 = Button(17)
+    	self.button1.when_pressed=self.on_button1
+    	self.button2.when_pressed=self.on_button2
+    	self.button3.when_pressed=self.on_button3
+    	
+    def on_button1(self):
+        model.dec()
+    	view.update()
     
-	def on_button2(self):
-		model.switch()
-		view.update()
+    def on_button2(self):
+    	model.switch()
+    	view.update()
     
-	def on_button3(self):
-		model.inc()
-		view.update()
+    def on_button3(self):
+    	model.inc()
+    	view.update()
 
 model = Model()
 view = View(model)
 controller = Controller(model, view)
-	
+    
 pause()
