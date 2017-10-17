7d0ae076ee02674483b11fbb3314255af9108545
Duco Fijma <d.fijma@activevideo.com>
2017-10-17 20:10:07 +0200

    glue my new rotary encode to actual GPIO pins

--- my_rotary_switch.py
+++ my_rotary_switch.py
@@ -17,12 +17,44 @@ class RotaryEncoder:
         self.button = button
         self.callback = callback
         self.state = self.START
+        
+        GPIO.setmode(GPIO.BCM)
+        
+        # The following lines enable the internal pull-up resistors
+        # on version 2 (latest) boards
+        GPIO.setwarnings(False)
+        GPIO.setup(self.pinA, GPIO.IN)
+        GPIO.setup(self.pinB, GPIO.IN)
+        GPIO.setup(self.button, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-    def event(self):
-        if (self.state != self.START):
-            print("shortcut in state: "+str(self.state))
+        # For version 1 (old) boards comment out the above four lines
+        # and un-comment the following 3 lines
+        #GPIO.setup(self.pinA, GPIO.IN)
+        #GPIO.setup(self.pinB, GPIO.IN)
+        #GPIO.setup(self.button, GPIO.IN)
+
+        # Add event detection to the GPIO inputs
+        GPIO.add_event_detect(self.pinA, GPIO.BOTH, callback=self.switch_event)
+        GPIO.add_event_detect(self.pinB, GPIO.BOTH, callback=self.switch_event)
+        GPIO.add_event_detect(self.button, GPIO.BOTH, callback=self.button_event, bouncetime=200)
+
+    def emit_rotary_event(self):
+        # if (self.state != self.START):
+        #    print("shortcut in state: "+str(self.state))
         self.callback(self.direction)
     
+    def switch_event(self, switch):
+        # read both rotary switches on both edges and feed to state machine
+        self.step(self.pinA, self.pinB)
+        
+    # Push button up event
+    def button_event(self,button):
+        if GPIO.input(button): 
+            event = self.BUTTONUP 
+        else:
+            event = self.BUTTONDOWN 
+        self.callback(event)
+
     def step(self, A, B): 
         # normal right click: 00 10 11 01
         # normal left click: 00 01 11 10
@@ -56,22 +88,22 @@ class RotaryEncoder:
                     # normal, seen 00 10 11 01
                     self.state = self.THREE
                 elif symbol == "00":
-                    self.event()
+                    self.emit_rotary_event()
                     self.state = self.START # skip THREE
                 elif symbol == "10":
-                    self.event()
+                    self.emit_rotary_event()
                     self.state = self.ONE # skip THREE and START
                 # else: ignore repeated 11    
             elif self.state == self.THREE:
                 if symbol == "00":
                     # normal, event and back to start
                     self.state = self.START
-                    self.event()
+                    self.emit_rotary_event()
                 elif symbol == "10":
-                    self.event()
+                    self.emit_rotary_event()
                     self.state = self.ONE # skip START
                 elif symbol == "11":
-                    self.event()
+                    self.emit_rotary_event()
                     self.state = self.TWO # skip ONE and STARt
                 # else: ignore repeated "01"
         
